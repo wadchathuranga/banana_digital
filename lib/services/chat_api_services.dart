7c67579ed7a04004c32e6274b9390a68070fb529
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2023-05-09 13:58:14 +0530

    unnecessary code removed

--- lib/services/chat_api_services.dart
+++ lib/services/chat_api_services.dart
@@ -10,45 +10,45 @@ import '../utils/app_configs.dart';
 class ChatApiServices {
 
   // send message
-  static Future<List<ChatModel>> sendMessage({ required String message, required String modelId }) async {
-    try {
-      var response = await http.post(
-        Uri.parse("$BASE_URL/completions"),
-        headers: {
-          "Authorization": "Bearer $OPENAI_API_KEY",
-          "Content-Type": "application/json"
-        },
-        body: jsonEncode({
-          "model": modelId,
-          "prompt": message,
-          "max_tokens": 100,
-        }),
-      );
-
-      Map jsonResponse = jsonDecode(response.body);
-
-      if (jsonResponse['error'] != null) {
-        throw HttpException(jsonResponse['error']['message']);
-      }
-
-      List<ChatModel> chatList = [];
-      if (jsonResponse["choices"].length > 0) {
-        // log("jsonResponse['choices']['text'] ${jsonResponse["choices"][0]["text"]}");
-        chatList = List.generate(
-          jsonResponse["choices"].length,
-              (index) => ChatModel(
-            msg: jsonResponse["choices"][index]["text"],
-            chatIndex: 1,
-          ),
-        );
-      }
-      return chatList;
-      // return ModelsModel.modelsFromSnapshot(temp);
-    } catch (error) {
-      log("Error $error");
-      rethrow;
-    }
-  }
+  // static Future<List<ChatModel>> sendMessage({ required String message, required String modelId }) async {
+  //   try {
+  //     var response = await http.post(
+  //       Uri.parse("$BASE_URL/completions"),
+  //       headers: {
+  //         "Authorization": "Bearer $OPENAI_API_KEY",
+  //         "Content-Type": "application/json"
+  //       },
+  //       body: jsonEncode({
+  //         "model": modelId,
+  //         "prompt": message,
+  //         "max_tokens": 100,
+  //       }),
+  //     );
+  //
+  //     Map jsonResponse = jsonDecode(response.body);
+  //
+  //     if (jsonResponse['error'] != null) {
+  //       throw HttpException(jsonResponse['error']['message']);
+  //     }
+  //
+  //     List<ChatModel> chatList = [];
+  //     if (jsonResponse["choices"].length > 0) {
+  //       // log("jsonResponse['choices']['text'] ${jsonResponse["choices"][0]["text"]}");
+  //       chatList = List.generate(
+  //         jsonResponse["choices"].length,
+  //             (index) => ChatModel(
+  //           msg: jsonResponse["choices"][index]["text"],
+  //           chatIndex: 1,
+  //         ),
+  //       );
+  //     }
+  //     return chatList;
+  //     // return ModelsModel.modelsFromSnapshot(temp);
+  //   } catch (error) {
+  //     log("Error $error");
+  //     rethrow;
+  //   }
+  // }
 
   // Send Message using ChatGPT API
   static Future<List<ChatModel>> sendMessageGPT(
---
+++ lib/utils/sample_configs.dart
@@ -0,0 +1,7 @@
+String BASE_URL = "https://api.openai.com/v1";
+String OPENAI_API_KEY = "xxxxxxxxx";
+
+
+
+// change file name from "sample_configs.dart" to "app_configs.dart"
+// then add your secret here before run the project
