77a97d1e68aa4195eb47ae148f504af7c28a0885
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2023-07-05 01:06:48 +0530

    minor issues fixed

--- lib/screens/C3_harvest_predict/HarvestPredictionHistoryScreen.dart
+++ lib/screens/C3_harvest_predict/HarvestPredictionHistoryScreen.dart
@@ -90,7 +90,7 @@ class _HarvestPredictionHistoryScreenState extends State<HarvestPredictionHistor
       body: Stack(
         children: [
           Padding(
-            padding: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.only(top: 5.0),
             child: Column(
               children: [
                 if (historyList.isNotEmpty)
@@ -125,7 +125,7 @@ class _HarvestPredictionHistoryScreenState extends State<HarvestPredictionHistor
 
   Widget _expandableTile(HarvestPredictionHistoryModel harvestPredictedHistory) {
     return Padding(
-      padding: const EdgeInsets.only(top: 15.0, left: 15.0, right: 15.0),
+      padding: const EdgeInsets.only(top: 10.0, left: 10.0, right: 10.0),
       child: Column(
         children: [
           ExpandableNotifier(
